b45587426f7ee764a14214393a0978d6f0cb3703
uudaykuma <uudaykuma@gmail.com>
2021-09-06 00:33:36 +0530

    progress bar

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -13,17 +13,20 @@ function getRandomArbitrary(min, max) {
   for (let i = 0; i < canvas.width; i += 5) {
     let ran = getRandomArbitrary(40, 60);
     ctx.fillRect(i, ran, 1, ran);
+    
   }
+let count = 0
 
 function dynamicProgressBar() {
   var audioDemo = document.getElementById('player');
   var elapsedTime = Math.round(audioDemo.currentTime);
   ctx.fillStyle = "red"
-  var fWidth = (elapsedTime / audioDemo.duration) * (canvas.clientWidth);
+  var fWidth = (elapsedTime / audioDemo.duration) * (canvas.clientWidth );
   console.log(fWidth)
+  count+= 5
   let ran = getRandomArbitrary(40, 60);
   if (fWidth > 0) {
-    ctx.fillRect(fWidth, 0, 1, ran);
+    ctx.fillRect(fWidth-10, ran, 1, ran);
   }
 }
 
@@ -31,27 +34,16 @@ function dynamicProgressBar() {
 function initEvents() {
   var audioDemo = document.getElementById('player');
 
-  //set up event to update the progress bar
-  audioDemo.addEventListener("timeupdate", dynamicProgressBar, true);
-  //set up mouse click to control position of audio
+
+  audioDemo.addEventListener("timeupdate", dynamicProgressBar);
   canvas.addEventListener("click", function(e) {
-    //this might seem redundant, but this these are needed later - make global to remove these
     
 
-    if (!e) {
-      e = window.event;
-    } //get the latest windows event if it isn't set
-    try {
-      //calculate the current time based on position of mouse cursor in canvas box
       audioDemo.currentTime = audioDemo.duration * (e.offsetX / canvas.clientWidth);
-    } catch (err) {
-      // Fail silently but show in F12 developer tools console
-      if (window.console && console.error("Error:" + err));
-    }
-  }, true);
+    
+  });
 }
-//this event gets fired when a page has loaded
-window.addEventListener("DOMContentLoaded", initEvents, false);
+initEvents()
 
 
 
